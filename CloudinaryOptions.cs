79b0ee5f315188e51ce97b69084d608bf9a48af6
pedrojuliolantigua1-art <pedrojuliolantigua1@gmail.com>
2026-06-20 23:41:22 -0400

    Borron de las clases y arreglado

--- CloudinaryOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SGA.Infrastructure.Almacenamiento
-{
-    public sealed class CloudinaryOptions
-    {
-        public string CloudName { get; set; } = string.Empty;
-        public string ApiKey { get; set; } = string.Empty;
-        public string ApiSecret { get; set; } = string.Empty;
-    }
-}
