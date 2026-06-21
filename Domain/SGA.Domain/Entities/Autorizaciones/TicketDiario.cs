e1b902f13333e2adf6ab9f3630fe50cc662d7d03
pedrojuliolantigua1-art <pedrojuliolantigua1@gmail.com>
2026-06-20 23:51:46 -0400

    Cambio de una clase

--- Domain/SGA.Domain/Entities/Autorizaciones/TicketDiario.cs
+++ Domain/SGA.Domain/Entities/Autorizaciones/TicketDiario.cs
@@ -1,13 +1,9 @@
 namespace SGA.Domain.Entities.Autorizaciones
 {
-    public class TicketMensual : AutorizacionTransporte
+    public class TicketDiario : AutorizacionTransporte
     {
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
     }
 
-    [Obsolete("Usar TicketMensual. Se mantiene para compatibilidad con codigo anterior.")]
-    public class TicketDiario : TicketMensual
-    {
-    }
 }
